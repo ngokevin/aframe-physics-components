75b55b9947db0e811daa9ebbf00db6b0dce852e4
Kevin Ngo <me@ngokevin.com>
2016-01-09 21:54:34 -0800

    basic working physics

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var worldComponent = {
     gravity: {
       // m/s^2
       type: 'vec3',
-      default: { x: 0, y: -9.8, z: 0 }
+      default: { x: 0, y: -9.82, z: 0 }
     }
   },
 
@@ -17,18 +17,24 @@ var worldComponent = {
     var fixedTimeStep = 1.0 / 60.0;
     var maxSubSteps = 3;
 
+    // Add simulation loop.
     this.el.addBehavior(function (time) {
       if (self.lastTime !== undefined){
-       var timeChange  = (time - self.lastTime) / 1000;
-       world.step(fixedTimeStep, timeChange, maxSubSteps);
+        var timeChange  = (time - self.lastTime) / 1000;
+        world.step(fixedTimeStep, timeChange, maxSubSteps);
+        world.bodies.forEach(function (body) {
+          if (body.aframeUpdate) { body.aframeUpdate(); }
+        });
       }
       self.lastTime = time;
     });
   },
 
   update: function (oldData) {
+    var gravity = this.data.gravity;
     var world = this.world;
-    world.gravity.set(this.data.gravity);
+    world.broadphase = new CANNON.NaiveBroadphase();
+    world.gravity.set(gravity.x, gravity.y, gravity.z);
   }
 }
 
@@ -43,24 +49,43 @@ var bodyComponent = {
     }
   },
 
+  /**
+   * TODO: Don't force physics-world to be on scene to allow for multiple physics worlds.
+   */
   init: function () {
     var self = this;
+    var sceneEl = this.el.sceneEl;
 
-    this.el.sceneEl.addEventListener('loaded', function () {
-      var world = self.world = self.el.sceneEl.components['physics-world'].world;
+    this.worldTickBehavior = this.worldTick.bind(this);
+
+    // Wait for scene to load to initialize physics world.
+    sceneEl.addEventListener('loaded', function () {
+      if (!('physics-world' in sceneEl.components)) {
+        console.warn('physics-world must be specified on scene for physics to work.');
+      }
+
+      var world = self.world = sceneEl.components['physics-world'].world;
+
       var position = self.el.getAttribute('position');
       var body = self.body = new CANNON.Body({
-        position: new CANNON.Vec3(position.x, position.y, position.z)
+        mass: self.data.mass,
+        position: new CANNON.Vec3(position.x, position.y, position.z),
+        shape: new CANNON.Sphere(1)  // TODO: IMPORT SHAPE FROM GEOMETRY.
       });
+      body.aframeUpdate = self.worldTickBehavior;
       world.add(body);
     });
   },
 
-  update: function (oldData) {
-    if (!this.world) { return; }
+  /**
+   * Copy CANNON rigid body properties to THREE object3D.
+   */
+  worldTick: function () {
     var body = this.body;
+    var el = this.el;
     var object3D = this.el.object3D;
-    body.position.copy(object3D.position);
+
+    el.setAttribute('position', body.position);
     body.quaternion.copy(object3D.quaternion);
   }
 };
